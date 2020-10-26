9b5abeb8a4cb7168d089e339f348881a5911daa6
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-26 13:29:06 -0700

    interfaces sources-readme file updated

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// typescript in 5 mins
-/*
-   TypeScript knows the JavaScript language and will generate types for you in many cases.
-   For example in creating a variable and assigning it to a particular value, TypeScript will use the value as its type.
-*/
-var helloWorld = "Hello World"; // if you hover on "helloworld" variable typeScript will suggest string type
+// Interface
+function printLabel(labeledObj) {
+    console.log(labeledObj.label);
+}
+var myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
+// Interface
 
-// typescript in 5 mins
+// function printLabel(labeledObj: { label: string }) {
+//   console.log(labeledObj.label);
+// }
 
+// let myObj = { label: "Size 10 Object" };
+// printLabel(myObj);
 
-/*
-   TypeScript knows the JavaScript language and will generate types for you in many cases.
-   For example in creating a variable and assigning it to a particular value, TypeScript will use the value as its type.
-*/
-let helloWorld = "Hello World"; // if you hover on "helloworld" variable typeScript will suggest string type
+// above example using interface
 
+interface LabeledValue {
+  label: string;
+}
 
+function printLabel(labeledObj: LabeledValue) {
+  console.log(labeledObj.label);
+}
+
+let myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
