0630a31a5f12304460730ecb50835f06af145ab3
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-27 20:49:52 -0700

    classes-object oriented programming paradigm

--- README.md
+++ README.md
@@ -2,35 +2,4 @@
 
 ## typescript-project-setup-for-learning-purpose
 
-steps:
- - type `` npm init -y ``
-  
- - give project name like ``typescript-learning `` in package json file but it should not be `` typescript `` in package.json file
-  
- - type `` npm i --save-dev -E typescript `` in terminal. This will install latest typescript (currently 4.0.5) in node modules_folder. `` E `` indicates Exact version because range versions are evil.
-  
- - create directory `` src `` in root and crate typescript file `` main.ts `` in src folder
-  
- - type some code in typescript file(main.ts) ``` 
-  let fname: string = "hello wao";
-console.log(fname); ```
-
-
-- in order to run ts file, first we need to compile into js because browser don't understand typescript. so to do, in package.json add `` build: "tsc" `` which is typescript compilation(tsc) in scripts. check package.json file, you'll understand
-
-- run `` npm run build `` got error
-
-- then run `` tsc ts-file-name ``
-  
-- in this project go to src folder `` cd src `` then run ``tsc main.ts`` and to see output, install node type definition package `` npm i --save-dev -E @types/node `` then type `` node main.ts `` in terminal to see output in terminal.  If you still get error then create tsconfig.json
-  
-- create tsconfig.json file in root directory, paste all things from tsconfig.json file
-
-- type `` npm run build `` to generate compiled js code in dist folder
-  
-- to see output in browser create index.html file in public directory and in html file link js file from generated dist folder.open html file in browser to see output
-
-  
-- want to track changes add `` watch: true `` in tsconfig file
-  
-- want to publish code in github make sure you create ``gitignore file`` to exclude node_modules folder
+## classes in typescript which is also Object Oriented Programming(OOP) Paradigm
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,111 @@
-let firstName: string = "hello wao"
-console.log(firstName);
+// classes
+/*
 
+class Greeter {
+  greeting: string;
+
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  greet() {
+    return "Hello, " + this.greeting;
+  }
+}
+
+let greeter = new Greeter("world");
+console.log(greeter.greet())
+
+// Inheritance
+
+class Animal {
+  move(distanceInMeters: number = 0) {
+    console.log(`Animal moved ${distanceInMeters}m.`);
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("Woof! Woof!");
+  }
+}
+
+const dog = new Dog();
+console.log(dog.bark());
+console.log(dog.move(10));
+console.log(dog.bark());
+
+class Animal {
+  name: string;
+  constructor(theName: string) {
+    this.name = theName;
+  }
+  move(distanceInMeters: number = 0) {
+    console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
+}
+
+class Snake extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters = 5) {
+    console.log("Slithering...");
+    super.move(distanceInMeters);
+  }
+}
+
+class Horse extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters = 45) {
+    console.log("Galloping...");
+    super.move(distanceInMeters);
+  }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+
+console.log(sam.move());
+console.log(tom.move(34));
+
+*/
+
+// Access Modifiers
+
+// class Animal {
+//   private name: string;
+
+//   constructor(theName: string) {
+//     this.name = theName;
+//   }
+// }
+
+// new Animal("Cat").name; //  Property 'name' is private and only accessible within class 'Animal'
+
+
+class Person {
+  protected name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Employee extends Person {
+  private department: string;
+
+  constructor(name: string, department: string) {
+    super(name);
+    this.department = department;
+  }
+
+  public getElevatorPitch() {
+    return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+  }
+}
+
+let howard = new Employee("Howard", "Sales");
+console.log(howard.getElevatorPitch());
+console.log(howard.name);
